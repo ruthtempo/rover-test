16bfa452d0d420a675498c1be201bea3ea7d5e86
Ruth <ruth@Deniss-Air.home>
2022-03-24 15:56:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,142 @@
+
+<template>
+  <div id="app">
+    <div>
+      <table>
+        <tr v-for="row in rows" :key="row">
+          <td v-for="col in columns" :key="col">
+            <p v-if="x === col && y === row">
+              ({{ x }}, {{ y }}), {{ orientation }}
+            </p>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <div>
+        <div>
+          <h4>current position</h4>
+          <div>
+            ({{ typeof x }}, {{ y }}), {{ orientation }} valid? : {{ isvalid }}
+          </div>
+        </div>
+        <h4>Commands</h4>
+        <ul>
+          <li>L : turn left</li>
+          <li>R : turn right</li>
+          <li>A : move foward</li>
+        </ul>
+      </div>
+      <div
+        style="display: flex; flex-direction: column; max-width:100px; column; margin: 10px"
+      >
+        <h4>Enter Coordinates</h4>
+        <label for="x"> x</label>
+        <input type="number" id="x" placeholder="enter number" v-model="x" />
+        <label for="y">y</label>
+        <input type="number" id="y" placeholder="enter number" v-model="y" />
+        <label for="coord">Orientation</label>
+        <input
+          type="text"
+          id="coord"
+          placeholder="N, S, W or E"
+          v-model="orientation"
+        />
+      </div>
+      <input type="text" placeholder="enter command" v-model="input" />
+
+      <button @click="move(input)">click</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      columns: [],
+      rows: [],
+      orientation: "N",
+      x: 1,
+      y: 2,
+      isvalid: true,
+      input: "",
+    };
+  },
+  created: function () {
+    this.createGrid();
+  },
+  methods: {
+    checkIfValid() {
+      if (this.x > 10 || this.x < 0 || this.y > 10 || this.y < 0) {
+        this.isvalid = false;
+      } else {
+        this.isvalid = true;
+      }
+    },
+    move(commands) {
+      commands.split("").forEach((command) => {
+        if (command === "A") {
+          this.moveFoward();
+        } else if (command === "L") {
+          this.turnLeft();
+        } else {
+          this.turnRight();
+        }
+      });
+      this.checkIfValid();
+    },
+    turnLeft() {
+      if (this.orientation === "N") {
+        this.orientation = "W";
+      } else if (this.orientation === "W") {
+        this.orientation = "S";
+      } else if (this.orientation === "S") {
+        this.orientation = "E";
+      } else if (this.orientation === "E") {
+        this.orientation = "N";
+      }
+    },
+    turnRight() {
+      if (this.orientation === "N") {
+        this.orientation = "E";
+      } else if (this.orientation === "W") {
+        this.orientation = "N";
+      } else if (this.orientation === "S") {
+        this.orientation = "W";
+      } else if (this.orientation === "E") {
+        this.orientation = "S";
+      }
+    },
+    moveFoward() {
+      if (this.orientation === "N") {
+        this.y = this.y + 1;
+      } else if (this.orientation === "S") {
+        this.y = this.y - 1;
+      } else if (this.orientation === "E") {
+        this.x = this.x + 1;
+      } else if (this.orientation === "W") {
+        this.x = this.x - 1;
+      }
+    },
+    createGrid() {
+      for (let i = 0; i < 10; i++) {
+        this.rows.push(i);
+        this.columns.push(i);
+      }
+    },
+    locateRover() {
+      if (this.column === this.x && this.row === this.z) {
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+td {
+  border: 1px solid black;
+  height: 50px;
+  width: 80px;
+}
+</style>
+
+
